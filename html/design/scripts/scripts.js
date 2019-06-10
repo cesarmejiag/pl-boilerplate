3a02ce5e044842e03993f9ae082f2cb53fedbb9c
César Mejía <iconfidence@gmail.com>
2019-06-10 00:01:45 -0500

    Typescript task before introduce tsconfig.json

--- html/design/scripts/scripts.js
+++ html/design/scripts/scripts.js
@@ -1,1 +1,1 @@
-var pl;(pl||(pl={})).sum=function(l,n){return l+n},console.log(pl.sum(1,2));
+define("script-2",["require","exports"],function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});t.default=function(e,t){return e+t}}),define("scripts",["require","exports","script-2"],function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),console.log(r.default(1,2))});
